2fafda30f9cdd3e5313a258868f38212862ebc7b
kijk2869 <mareowst@gmail.com>
2020-06-21 22:28:05 +0900

    Update client.py

--- discodo/client.py
+++ discodo/client.py
@@ -1,13 +1,32 @@
+import socket
 import asyncio
 
 
 class VoiceClient:
-    def __init__(self, guild_id, channel_id):
+    def __init__(self, client, guild_id, data):
         self.loop = asyncio.get_event_loop()
         self.ws = None
+        self.socket = None
 
+        self.client = client
         self.guild_id = guild_id
-        self.channel_id = channel_id
 
-    async def connect(self, reconnect=True):
+        self.token = data.get('token')
+        endpoint = data.get('endpoint')
+        self.endpoint = endpoint.replace(':80', '')
+        self.endpointIp = socket.gethostbyname(self.endpoint)
+
+    async def create(self, reconnect=True):
         raise NotImplementedError
+    
+    async def createSocket(self):
+        self.session_id = self.client.session_id
+        
+        if self.socket:
+            try:
+                self.socket.close()
+            except:
+                pass
+    
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.socket.setblocking = False
