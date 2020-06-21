8f6e66e7c52a1bb974c9e99b4bad1b5b83f4370d
kijk2869 <mareowst@gmail.com>
2020-06-21 13:21:46 +0900

    Memory Leak Solve

--- discodo/natives/AudioFifo.py
+++ discodo/natives/AudioFifo.py
@@ -1,9 +1,16 @@
 import av
+import os
 
+AUDIOBUFFERLIMIT = os.getenv('AUDIOBUFFERLIMIT', 30)
+AUDIOBUFFERLIMITMS = AUDIOBUFFERLIMIT * 1000
 
 class AudioFifo(av.AudioFifo):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+    
+    @property
+    def haveToFillBuffer(self):
+        return self.samples < AUDIOBUFFERLIMITMS
 
     def read(self, samples=20):
         AudioFrame = super().read(samples)
