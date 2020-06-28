1f76b2e4b39a09677f2fc05c49147dc799d28a6c
kijk2869 <kijk2869@users.noreply.github.com>
2020-06-28 05:06:00 +0000

    Adjusted files for PEP-8 compliance

--- discodo/extractor.py
+++ discodo/extractor.py
@@ -5,7 +5,9 @@ from youtube_dl import YoutubeDL as YoutubeDLClient
 
 log = logging.getLogger("discodo.extractor")
 
-YOUTUBE_PLAYLIST_ID_REGEX = Regex(r'(?:http|https|)(?::\/\/|)(?:www.|)(?:music.|)(?:youtu\.be\/|youtube\.com(?:\/embed\/|\/v\/|\/watch\?v=|\/ytscreeningroom\?v=|\/feeds\/api\/videos\/|\/user\S*[^\w\-\s]|\S*[^\w\-\s]))([\w\-]{12,})[a-z0-9;:@#?&%=+\/\$_.-]*(?:&index=|)([0-9]*)?')
+YOUTUBE_PLAYLIST_ID_REGEX = Regex(
+    r'(?:http|https|)(?::\/\/|)(?:www.|)(?:music.|)(?:youtu\.be\/|youtube\.com(?:\/embed\/|\/v\/|\/watch\?v=|\/ytscreeningroom\?v=|\/feeds\/api\/videos\/|\/user\S*[^\w\-\s]|\S*[^\w\-\s]))([\w\-]{12,})[a-z0-9;:@#?&%=+\/\$_.-]*(?:&index=|)([0-9]*)?')
+
 
 def _extract(query):
     option = {
@@ -23,18 +25,19 @@ def _extract(query):
     YoutubePlaylistMatch = YOUTUBE_PLAYLIST_ID_REGEX.match(query)
     if YoutubePlaylistMatch:
         if YoutubePlaylistMatch.group(1).startswith(('RD', 'UL', 'PU')):
-            option['playlist_items'] = 25 # handle Youtube Mix Playlist
+            option['playlist_items'] = 25  # handle Youtube Mix Playlist
 
-        option['playliststart'] = int(YoutubePlaylistMatch.group(2)) if YoutubePlaylistMatch.group(2).isdigit() else 1
+        option['playliststart'] = int(YoutubePlaylistMatch.group(
+            2)) if YoutubePlaylistMatch.group(2).isdigit() else 1
         option['dump_single_json'] = True
         option['extract_flat'] = True
     else:
         option['noplaylist'] = True
-    
+
     YoutubeDL = YoutubeDLClient(option)
     Data = YoutubeDL.extract_info(query, download=False)
 
     if 'entries' in Data:
         Data = Data['entries']
-    
+
     return Data
