ae391e6cebccbd8f46da32a083952e97a9b0058e
kijk2869 <mareowst@gmail.com>
2020-07-03 00:37:44 +0900

    Update websocket.py

--- discodo/websocket.py
+++ discodo/websocket.py
@@ -17,18 +17,22 @@ app = Blueprint(__name__)
 
 
 @app.websocket('/')
-async def feed(request, ws):
-    log.info(f'create new websocket connection from {request.socket}.')
+async def feedStarter(request, ws):
+    Event = asyncio.Event()
+    Future = request.app.loop.create_task(WebsocketFeeder().feed(request, ws, Event))
+    await Event.wait()
 
-    if not hasattr(request.app, 'Nodes'):
-        request.app.Nodes = {}
-    _Node = None
+class WebsocketFeeder:
+    def __init__(self):
+        self._Node = None
+        self.request = None
+        self.ws = None
 
-    async def sendJson(Data):
-        log.debug(f'send to {request.socket}: {Data}')
-        return await ws.send(json.dumps(Data))
-
-    async def initial_connection():
+    async def sendJson(self, Data):
+        log.debug(f'send to {self.request.socket}: {Data}')
+        return await self.ws.send(json.dumps(Data))
+            
+    async def initial_connection(self):
         payload = {
             'op': 'INITIAL_CONNECTION',
             'd': {
@@ -38,76 +42,89 @@ async def feed(request, ws):
             }
         }
 
-        await sendJson(payload)
-
-    async def authentication_failed():
+        await self.sendJson(payload)
+    
+    async def authentication_failed(self):
         payload = {
             'op': 'AUTHENTICATION_FAILED',
             'd': 'Password mismatch.'
         }
 
-        await sendJson(payload)
-
-    async def discodoEvent(guild_id, event, *args, **kwargs):
+        await self.sendJson(payload)
+    
+    async def discodoEvent(self, guild_id, event, *args, **kwargs):
         payload = {
             'op': event,
             'guild_id': guild_id,
             'd': kwargs
         }
 
-        await sendJson(payload)
-
-    async def identify(Data):
-        if Data['user_id'] in request.app.Nodes:
+        await self.sendJson(payload)
+    
+    async def identify(self, Data):
+        if Data['user_id'] in self.request.app.Nodes:
             payload = {
                 'op': 'IDENTIFIED',
                 'd': 'Node already initialized.'
             }
         else:
-            request.app.Nodes[Data['user_id']] = Node(
+            self.request.app.Nodes[Data['user_id']] = Node(
                 user_id=Data['user_id'], session_id=Data.get('session_id'))
-            request.app.Nodes[Data['user_id']].event.onAny(discodoEvent)
+            self.request.app.Nodes[Data['user_id']].event.onAny(self.discodoEvent)
 
             payload = {
                 'op': 'IDENTIFIED',
                 'd': 'Node initialized.'
             }
-        _Node = request.app.Nodes[Data['user_id']]
-        await sendJson(payload)
-
-    async def heartbeat(Data):
+        self._Node = self.request.app.Nodes[Data['user_id']]
+        await self.sendJson(payload)
+    
+    async def heartbeat(self, Data):
         payload = {
             'op': 'HEARTBEAT_ACK',
             'd': Data
         }
 
-        await sendJson(payload)
+        await self.sendJson(payload)
+    
+    async def discord_event(self, Data):
+        if self._Node:
+            self._Node.discordDispatch(Data)
 
-    async def discord_event(Data):
-        if _Node:
-            _Node.discordDispatch(Data)
+    async def feed(self, request, ws, _Event):
+        log.info(f'create new websocket connection from {request.socket}.')
 
-    if request.headers.get('Authorization') != PASSWORD:
-        log.warning(f'websocket connection from {request.socket} password mismatch.')
-        await authentication_failed()
-        await ws.close(4004)
+        self.request, self.ws = request, ws
 
-    await initial_connection()
+        if not hasattr(request.app, 'Nodes'):
+            request.app.Nodes = {}
 
-    while True:
-        try:
-            Data = json.loads(await asyncio.wait_for(ws.recv(), timeout=WEBSOCKETTIMEOUT))
-        except (ConnectionClosed, asyncio.TimeoutError):
-            log.info(f'connection from {request.socket} error, closing..')
+        if request.headers.get('Authorization') != PASSWORD:
+            log.warning(f'websocket connection from {request.socket} password mismatch.')
+            await self.authentication_failed()
 
-            await ws.close(4000)
+            return await ws.close(4004)
 
-        Operation, Data = Data['op'], Data['d']
-        log.debug(f'recieve from {request.socket}: {Operation} - {Data}')
+        await self.initial_connection()
 
-        if Operation == 'IDENTIFY':
-            await identify(Data)
-        if Operation == 'HEARTBEAT':
-            await heartbeat(Data)
-        if Operation == 'DISCORD_EVENT':
-            await discord_event(Data)
+        while True:
+            try:
+                JSONData = json.loads(await asyncio.wait_for(ws.recv(), timeout=WEBSOCKETTIMEOUT))
+            except (ConnectionClosed, asyncio.TimeoutError):
+                log.info(f'connection from {request.socket} error, closing..')
+
+                if self._Node:
+                    del request.app.Nodes[self._Node.user_id]
+                    self._Node = None
+
+                return await ws.close(4000)
+
+            Operation, Data = JSONData['op'], JSONData['d']
+            log.debug(f'recieve from {request.socket}: {Operation} - {Data}')
+
+            if Operation == 'IDENTIFY':
+                await self.identify(Data)
+            if Operation == 'HEARTBEAT':
+                await self.heartbeat(Data)
+            if Operation == 'DISCORD_EVENT':
+                await self.discord_event(Data)
