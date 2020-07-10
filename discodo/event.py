5b0d0b7be5850374d7df66eed00eae190b25f5d3
kijk2869 <mareowst@gmail.com>
2020-07-10 23:49:15 +0900

    Server Roll back

--- discodo/event.py
+++ discodo/event.py
@@ -32,7 +32,9 @@ class DiscordEvent:
         self.client.session_id = data['session_id']
 
     async def parseVoiceStateUpdate(self, data):
-        return
+        if data["user_id"] != str(self.client.user_id): return
+        log.info(f'recieve self voice update. set session id {data["session_id"]}')
+        self.client.session_id = data['session_id']
 
     async def parseVoiceServerUpdate(self, data):
         if int(data['guild_id']) in self.client.voiceClients:
