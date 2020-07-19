5ffd4f20cbe8da4c8fd3c1a6a5c2d139b2166dbd
kijk2869 <kijk2869@users.noreply.github.com>
2020-07-19 09:39:53 +0000

    Adjusted files for PEP-8 compliance

--- discodo/stat.py
+++ discodo/stat.py
@@ -3,20 +3,26 @@ import psutil
 
 Process = psutil.Process(os.getpid())
 
+
 def getProcessMemory():
     return round(Process.memory_info().rss / 1e+6)
+
 
 def getProcessCpu():
     return Process.cpu_percent()
 
+
 def getTotalCpu():
     return psutil.cpu_percent()
+
 
 def getMemory():
     return round(psutil.virtual_memory().total / 1e+6)
 
+
 def getCpuCount():
     return psutil.cpu_count()
+
 
 def getStat():
     return {
