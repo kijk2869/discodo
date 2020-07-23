c4e2679a357f3dcf416f89b7d7377e19e611d767
kijk2869 <kijk2869@users.noreply.github.com>
2020-07-23 16:43:16 +0000

    Adjusted files for PEP-8 compliance

--- discodo/node/server/server.py
+++ discodo/node/server/server.py
@@ -14,5 +14,6 @@ app.register_blueprint(WebsocketBlueprint)
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
-    loop.create_task(app.create_server(host='0.0.0.0', return_asyncio_server=True))
+    loop.create_task(app.create_server(
+        host='0.0.0.0', return_asyncio_server=True))
     loop.run_forever()
