a381f11fcb79cb40402303f6c4bb6af63ebb5c03
kijk2869 <mareowst@gmail.com>
2020-07-24 21:31:39 +0900

    Create expiremental.py

---
+++ discodo/expiremental.py
@@ -0,0 +1,23 @@
+import sys
+import math
+import numpy as np
+
+def volumeDetect(Data):
+    volume = audioop.rms(Data,2)
+    return volume
+
+def volumeNorm(Data):
+    if not Data:
+        return
+
+    try:
+        detected = volumeDetect(Data)
+        sys.stdout.write("" + str(detected) + '|' + '#' * round(detected / 150) + '\n')
+        a = (7000 / detected)
+        print(a)
+        _Data = audioop.mul(Data, 2, a)
+        b = volumeDetect(Data)
+        sys.stdout.write("->" + str(b) + '|' + '#' * round(b / 150) + '\n')
+        return _Data
+    except:
+        return Data
