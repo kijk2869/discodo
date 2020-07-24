c610a2ce7b2d08d14ed99fb7452f40f52655ac19
kijk2869 <mareowst@gmail.com>
2020-07-25 02:13:19 +0900

    add Timeout

--- discodo/node/client/voice_client.py
+++ discodo/node/client/voice_client.py
@@ -21,59 +21,59 @@ class VoiceClient:
     async def loadSong(self, Query):
         await self.send("loadSong", {"query": Query})
 
-        return await self.emitter.wait_for("loadSong")
+        return await self.emitter.wait_for("loadSong", timeout=10.0)
 
     async def skip(self, offset=1):
         await self.send("skip", {"offset": offset})
 
-        return await self.emitter.wait_for("skip")
+        return await self.emitter.wait_for("skip", timeout=10.0)
 
     async def seek(self, offset):
         await self.send("seek", {"offset": offset})
 
-        return await self.emitter.wait_for("seek")
+        return await self.emitter.wait_for("seek", timeout=10.0)
 
     async def setVolume(self, volume):
         await self.send("setVolume", {"volume": volume})
 
-        return await self.emitter.wait_for("setVolume")
+        return await self.emitter.wait_for("setVolume", timeout=10.0)
 
     async def setCrossfade(self, crossfade):
         await self.send("setCrossfade", {"crossfade": crossfade})
 
-        return await self.emitter.wait_for("setCrossfade")
+        return await self.emitter.wait_for("setCrossfade", timeout=10.0)
 
     async def setAutoplay(self, autoplay):
         await self.send("setAutoplay", {"autoplay": autoplay})
 
-        return await self.emitter.wait_for("setAutoplay")
+        return await self.emitter.wait_for("setAutoplay", timeout=10.0)
 
     async def setFilter(self, filter):
         await self.send("setFilter", {"filter": filter})
 
-        return await self.emitter.wait_for("setFilter")
+        return await self.emitter.wait_for("setFilter", timeout=10.0)
 
     async def getQueue(self):
         await self.send("getQueue")
 
-        return await self.emitter.wait_for("Queue")
+        return await self.emitter.wait_for("Queue", timeout=10.0)
 
     async def getCurrent(self):
         await self.send("getCurrent")
 
-        return await self.emitter.wait_for("Current")
+        return await self.emitter.wait_for("Current", timeout=10.0)
 
     async def shuffle(self):
         await self.send("shuffle")
 
-        return await self.emitter.wait_for("shuffle")
+        return await self.emitter.wait_for("shuffle", timeout=10.0)
 
     async def remove(self, index):
         await self.send("remove", {"index": index})
 
-        return await self.emitter.wait_for("remove")
+        return await self.emitter.wait_for("remove", timeout=10.0)
 
     async def destroy(self):
         await self.send("VC_DESTROY")
 
-        return await self.emitter.wait_for("VC_DESTROYED")
+        return await self.emitter.wait_for("VC_DESTROYED", timeout=10.0)
