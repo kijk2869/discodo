8f379603a2a7b49d27ae77b38fa72533b6ffed57
kijk2869 <kijk2869@users.noreply.github.com>
2020-07-24 15:38:24 +0000

    Adjusted files for PEP-8 compliance

--- discodo/node/server/server.py
+++ discodo/node/server/server.py
@@ -13,21 +13,25 @@ app = Sanic(__name__)
 
 app.register_blueprint(WebsocketBlueprint)
 
+
 def authorized(func):
     def wrapper(request, *args, **kwargs):
         if request.headers.get('Authorization') != PASSWORD:
             abort(403, 'Password mismatch.')
-        
+
         return func(request, *args, **kwargs)
     return wrapper
+
 
 @app.route('/')
 async def index(request):
     return response.json({'hello': 'discodo'})
 
+
 @app.route('/stat')
 async def stat(request):
     return response.json(getStat())
+
 
 @app.route('/getSong')
 @authorized
@@ -36,5 +40,5 @@ async def getSong(request):
 
     if not query:
         abort(400, 'Missing parameter query.')
-    
+
     return response.json(await AudioData.create(query))
