715b5091e5e7154307c911d6b32327638e6fe50e
kijk2869 <mareowst@gmail.com>
2020-07-25 00:37:19 +0900

    Update server.py

--- discodo/node/server/server.py
+++ discodo/node/server/server.py
@@ -1,9 +1,40 @@
-import asyncio
-
-from sanic import Sanic
+import os
+from sanic import Sanic, response
+from sanic.exceptions import abort
+from functools import wraps
 
 from .websocket import app as WebsocketBlueprint
+from ...AudioSource import AudioData
+from ...stat import getStat
+
+PASSWORD = os.getenv("PASSWORD", "hellodiscodo")
 
 app = Sanic(__name__)
 
 app.register_blueprint(WebsocketBlueprint)
+
+def authorized(func):
+    def wrapper(request, *args, **kwargs):
+        if request.headers.get('Authorization') != PASSWORD:
+            abort(403, 'Password mismatch.')
+        
+        return func(request, *args, **kwargs)
+    return wrapper
+
+@app.route('/')
+async def index(request):
+    return response.json({'hello': 'discodo'})
+
+@app.route('/stat')
+async def stat(request):
+    return response.json(getStat())
+
+@app.route('/getSong')
+@authorized
+async def getSong(request):
+    query = ''.join(request.args.get('query', [])).strip()
+
+    if not query:
+        abort(400, 'Missing parameter query.')
+    
+    return response.json(await AudioData.create(query))
