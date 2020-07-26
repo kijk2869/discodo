52ae29aaf3a6446ce075057aae27e15ad8180a0b
kijk2869 <kijk2869@users.noreply.github.com>
2020-07-26 02:56:22 +0000

    Adjusted files for PEP-8 compliance

--- discodo/DPYClient.py
+++ discodo/DPYClient.py
@@ -3,10 +3,11 @@ import discord
 from itertools import chain
 from .node.client import Node as NodeClient
 
+
 class DPYClient:
     def __init__(self, client):
         self.client = client
-    
+
         self.Nodes = []
 
     def register_node(self, *args, **kwargs):
@@ -17,13 +18,13 @@ class DPYClient:
         Node.event.on('VC_DESTROYED', self._node_destroyed)
 
         return self
-    
+
     async def _node_destroyed(self, Data):
         guild = self.client.get_guild(int(Data['guild_id']))
         ws = self.__get_websocket(guild.shard_id)
 
         await ws.voice_state(guild.id, None)
-    
+
     async def getBestNode(self):
         async def getWithNodeData(self, Node):
             Data = await Node.getStat()
@@ -37,10 +38,11 @@ class DPYClient:
         )
 
         Stats = [Future.result() for Future in Futures if Future.result()]
-        SortedWithPerformance = sorted(Stats, key=lambda x: x['TotalPlayers'], reverse=True)
+        SortedWithPerformance = sorted(
+            Stats, key=lambda x: x['TotalPlayers'], reverse=True)
 
         return SortedWithPerformance[0]
-    
+
     @property
     def voiceClients(self):
         return {
@@ -54,35 +56,35 @@ class DPYClient:
                 )
             )
         }
-    
+
     def getVC(self, guildID):
         return self.voiceClients.get(guildID)
-    
+
     def __get_websocket(self, id):
         if isinstance(self.client, discord.AutoShardedClient):
             return self.client.shards[id].ws
         elif not self.client.shard_id or self.client.shard_id == id:
             return self.client.ws
-    
+
     async def connect(self, channel):
         if not hasattr(channel, 'guild'):
             raise ValueError
-        
+
         ws = self.__get_websocket(channel.guild.shard_id)
 
         await ws.voice_state(channel.guild.id, channel.id)
-    
+
     async def disconnect(self, guild):
         ws = self.__get_websocket(guild.shard_id)
 
         await ws.voice_state(guild.id, None)
-    
+
     async def destroy(self, guild):
         if not guild.id in self.voiceClients:
             raise ValueError
-            
+
         vc = self.getVC(guild.id)
         ws = self.__get_websocket(guild.shard_id)
-        
+
         await ws.voice_state(guild.id, None)
         await vc.destroy()
