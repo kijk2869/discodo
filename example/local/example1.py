791ceccc00a87c0d0c804a5ab4af9e7ec1bb76e8
kijk2869 <mareowst@gmail.com>
2020-08-03 09:06:47 +0900

    add: local audio-hosted example

--- example/local/example1.py
+++ example/local/example1.py
@@ -4,6 +4,10 @@ This example isn't recommended in production.
 Just use it only to learn the structure.
 This code is not perfect and can be complex.
 
+
+Additionally, remote is recommended for efficient performance management.
+Just know there is a way to do this.
+
 """
 
 
@@ -16,8 +20,7 @@ import discodo
 logging.basicConfig(level=logging.INFO)
 
 app = discord.Client()
-Audio = discodo.DPYClient(app)
-Audio.register_node("ws://yourwonderfulnode:prettyport", password="hellodiscodo")
+Audio = discodo.AudioManager()
 
 
 @app.event
@@ -25,20 +28,25 @@ async def on_ready():
     print("bot is now ready.")
 
 @app.event
+async def on_socket_response(payload):
+    Audio.discordDispatch(payload)
+
+
+@app.event
 async def on_message(message):
     await app.wait_until_ready()
-
+    
     if message.content.startswith("!join"):
         if not message.author.voice:
             return await message.channel.send("Join the voice channel first.")
 
-        await Audio.connect(message.author.voice.channel)
+        await app.ws.voice_state(message.guild.id, message.author.voice.channel.id)
         return await message.channel.send(
             f"connected to {message.author.voice.channel.mention}."
         )
 
     if message.content.startswith("!stop"):
-        vc = Audio.getVC(message.guild)
+        vc = Audio.getVC(message.guild.id)
 
         if not vc:
             return await message.channel.send("Please type `!join` first.")
@@ -48,7 +56,7 @@ async def on_message(message):
         return await message.channel.send(f"player stopped and cleaned the queue.")
 
     if message.content.startswith("!play"):
-        vc = Audio.getVC(message.guild)
+        vc = Audio.getVC(message.guild.id)
 
         if not vc:
             return await message.channel.send("Please type `!join` first.")
@@ -57,37 +65,25 @@ async def on_message(message):
 
         if isinstance(Song, list):
             return await message.channel.send(
-                f'{len(Song) - 1} songs except {Song[0]["title"]} added.'
+                f'{len(Song) - 1} songs except {Song[0].title} added.'
             )
         else:
-            return await message.channel.send(f'{Song["title"]} added.')
+            return await message.channel.send(f'{Song.title} added.')
 
     if message.content.startswith("!skip"):
-        vc = Audio.getVC(message.guild)
+        vc = Audio.getVC(message.guild.id)
 
         if not vc:
             return await message.channel.send("Please type `!join` first.")
 
         offset = int(message.content[5:].strip()) if message.content[5:].strip() else 1
 
-        Remain = await vc.skip(offset)
+        vc.skip(offset)
 
-        return await message.channel.send(f"{offset} skipped. {Remain}songs remain")
-
-    if message.content.startswith("!remove"):
-        vc = Audio.getVC(message.guild)
-
-        if not vc:
-            return await message.channel.send("Please type `!join` first.")
-
-        offset = int(message.content[7:].strip()) if message.content[7:].strip() else 1
-
-        Data = await vc.remove(offset)
-
-        return await message.channel.send(f'{Data["removed"]["title"]} removed.')
+        return await message.channel.send(f"{offset} skipped.")
 
     if message.content.startswith("!volume"):
-        vc = Audio.getVC(message.guild)
+        vc = Audio.getVC(message.guild.id)
 
         if not vc:
             return await message.channel.send("Please type `!join` first.")
@@ -96,12 +92,12 @@ async def on_message(message):
             int(message.content[7:].strip()) if message.content[7:].strip() else 100
         )
 
-        Volume = await vc.setVolume(offset)
+        Volume = vc.setVolume(offset / 100)
 
-        return await message.channel.send(f"set volume to {Volume}%.")
+        return await message.channel.send(f"set volume to {Volume * 100}%.")
 
     if message.content.startswith("!crossfade"):
-        vc = Audio.getVC(message.guild)
+        vc = Audio.getVC(message.guild.id)
 
         if not vc:
             return await message.channel.send("Please type `!join` first.")
@@ -110,14 +106,14 @@ async def on_message(message):
             int(message.content[10:].strip()) if message.content[10:].strip() else 10
         )
 
-        Crossfade = await vc.setCrossfade(offset)
+        Crossfade = vc.setCrossfade(offset)
 
         return await message.channel.send(
             f"set crossfade seconds to {Crossfade} seconds."
         )
 
     if message.content.startswith("!autoplay"):
-        vc = Audio.getVC(message.guild)
+        vc = Audio.getVC(message.guild.id)
 
         if not vc:
             return await message.channel.send("Please type `!join` first.")
@@ -127,14 +123,14 @@ async def on_message(message):
         )
         offset = {"on": True, "off": False}.get(offset, True)
 
-        autoplay = await vc.setAutoplay(offset)
+        autoplay = vc.setAutoplay(offset)
 
         return await message.channel.send(
             f'auto related play {"enabled" if autoplay else "disabled"}.'
         )
 
     if message.content.startswith("!repeat"):
-        vc = Audio.getVC(message.guild)
+        vc = Audio.getVC(message.guild.id)
 
         if not vc:
             return await message.channel.send("Please type `!join` first.")
@@ -144,26 +140,24 @@ async def on_message(message):
         )
         offset = {"on": True, "off": False}.get(offset, True)
 
-        repeat = await vc.setRepeat(offset)
+        repeat = vc.setRepeat(offset)
 
         return await message.channel.send(
             f'repeat {"enabled" if repeat else "disabled"}.'
         )
 
     if message.content.startswith("!np"):
-        vc = Audio.getVC(message.guild)
+        vc = Audio.getVC(message.guild.id)
 
         if not vc:
             return await message.channel.send("Please type `!join` first.")
 
-        Data = await vc.getState()
-
         return await message.channel.send(
-            f'Now playing: {Data["current"]["title"]} `{Data["position"]["duration"]}:{Data["current"]["duration"]}`'
+            f'Now playing: {vc.player.current.title} `{vc.player.current.duration}:{vc.player.current.AudioData.duration}`'
         )
 
     if message.content.startswith("!shuffle"):
-        vc = Audio.getVC(message.guild)
+        vc = Audio.getVC(message.guild.id)
 
         if not vc:
             return await message.channel.send("Please type `!join` first.")
@@ -173,20 +167,18 @@ async def on_message(message):
         return await message.channel.send("shuffle the queue.")
 
     if message.content.startswith("!queue"):
-        vc = Audio.getVC(message.guild)
+        vc = Audio.getVC(message.guild.id)
 
         if not vc:
             return await message.channel.send("Please type `!join` first.")
 
-        State = await vc.getState()
-        Queue = await vc.getQueue()
         QueueText = "\n".join(
-            [str(Queue.index(Item) + 1) + ". " + Item["title"] for Item in Queue]
+            [str(vc.Queue.index(Item) + 1) + ". " + Item.title for Item in vc.Queue]
         )
 
         return await message.channel.send(
             f"""
-Now playing: {State["current"]["title"]} `{State["position"]["duration"]}:{State["current"]["duration"]}`
+Now playing: {vc.player.current.title} `{vc.player.current.duration}:{vc.player.current.AudioData.duration}`
 
 {QueueText}
 """
@@ -201,9 +193,11 @@ Now playing: {State["current"]["title"]} `{State["position"]["duration"]}:{State
         offset = (
             int(message.content[10:].strip()) if message.content[10:].strip() else 0
         )
-        filter = discodo.equalizer.bassboost(offset) if offset != 0 else {}
+        filter = (
+            {"anequalizer": discodo.equalizer.bassboost(offset)} if offset != 0 else {}
+        )
 
-        await vc.setFilter(filter)
+        vc.setFilter(filter)
 
         return await message.channel.send(f"set bassboost level {offset}%.")
 
@@ -217,7 +211,7 @@ Now playing: {State["current"]["title"]} `{State["position"]["duration"]}:{State
             float(message.content[6:].strip()) if message.content[6:].strip() else 1.0
         )
 
-        await vc.setFilter({"atempo": str(offset)})
+        vc.setFilter({"atempo": str(offset)})
 
         return await message.channel.send(f"set tempo to {offset}.")
 
@@ -229,7 +223,7 @@ Now playing: {State["current"]["title"]} `{State["position"]["duration"]}:{State
 
         offset = int(message.content[5:].strip()) if message.content[5:].strip() else 1
 
-        await vc.seek(offset)
+        vc.seek(offset)
 
         return await message.channel.send(f"seek to {offset}.")
 
