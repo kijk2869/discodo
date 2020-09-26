049abe98ca16e8d7b750f049feee850b105e1230
kijk2869 <mareowst@gmail.com>
2020-09-26 22:32:14 +0900

    feat(client): voice client

--- discodo/client/voice_client.py
+++ discodo/client/voice_client.py
@@ -1,6 +1,3 @@
-import asyncio
-from discodo.utils.eventDispatcher import EventDispatcher
-
 from ..utils import EventDispatcher
 
 
@@ -21,3 +18,66 @@ class VoiceClient:
         Data["guild_id"] = self.guild_id
 
         return await self.Node.send(Operation, Data)
+
+    async def query(
+        self, Operation: str, Data: dict = {}, Event: str = None, timeout: float = 10.0
+    ) -> dict:
+        if not Event:
+            Event = Operation
+
+        Future = self.loop.create_task(
+            self.emitter.wait_for(
+                Event,
+                condition=lambda Data: Data["guild_id"] == self.guild_id,
+                timeout=timeout,
+            )
+        )
+
+        await self.send(Operation, Data)
+
+        return await Future
+
+    async def loadSource(self, Query: str) -> dict:
+        return (await self.query("loadSource", {"query": Query}))["source"]
+
+    async def putSource(self, Source: dict) -> int:
+        return (await self.query("putSource", {"song": Source}))["index"]
+
+    async def skip(self, offset: int = 1) -> int:
+        return (await self.query("skip", {"offset": offset}))["remain"]
+
+    async def seek(self, offset: float) -> dict:
+        return await self.query("seek", {"offset": offset})
+
+    async def setVolume(self, volume: int) -> float:
+        return (await self.query("setVolume", {"volume": volume}))["volume"]
+
+    async def setCrossfade(self, crossfade: float) -> float:
+        return (await self.query("setCrossfade", {"crossfade": crossfade}))["crossfade"]
+
+    async def setAutoplay(self, autoplay: bool) -> bool:
+        return (await self.query("setAutoplay", {"autoplay": autoplay}))["autoplay"]
+
+    async def setGapless(self, gapless: bool) -> bool:
+        return (await self.query("setGapless", {"gapless": gapless}))["gapless"]
+
+    async def setFilter(self, filter: dict) -> dict:
+        return await self.query("setFilter", {"filter": filter})
+
+    async def pause(self) -> dict:
+        return await self.query("pause")
+
+    async def resume(self) -> dict:
+        return await self.query("resume")
+
+    async def getQueue(self) -> list:
+        return (await self.query("getQueue", Event="Queue"))["entries"]
+
+    async def getState(self) -> dict:
+        return await self.query("getState", Event="State")
+
+    async def shuffle(self) -> dict:
+        return await self.query("shuffle")
+
+    async def remove(self, index: int) -> dict:
+        return await self.query("remove", {"index": index})
