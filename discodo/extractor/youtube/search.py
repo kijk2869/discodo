9ce58eb82fb7716d913d830b7b373b427082ba39
kijk2869 <mareowst@gmail.com>
2020-10-28 14:51:28 +0900

    feat(youtube): add subtitle parser

--- discodo/extractor/youtube/search.py
+++ discodo/extractor/youtube/search.py
@@ -63,4 +63,4 @@ if __name__ == "__main__":
 
     import asyncio
 
-    print(asyncio.run(search("dazbee")))
+    print(asyncio.run(search("")))
---
+++ discodo/extractor/youtube/subtitle.py
@@ -0,0 +1,45 @@
+import urllib.parse
+from xml.etree import ElementTree
+
+import aiohttp
+
+
+async def get_subtitle(videoId: str) -> list:
+    async with aiohttp.ClientSession() as session:
+        async with session.get(
+            "https://video.google.com/timedtext",
+            params={"hl": "en", "type": "list", "v": videoId},
+        ) as resp:
+            Body: str = await resp.text()
+
+    Tree = ElementTree.fromstring(Body)
+
+    Subtitles: dict = {}
+
+    for Track in Tree.findall("track"):
+        if Track.attrib["lang_code"] in Subtitles:
+            continue
+
+        Params = urllib.parse.urlencode(
+            {
+                "lang": Track.attrib["lang_code"],
+                "v": videoId,
+                "fmt": "srv1",
+                "name": Track.attrib["name"].encode("utf-8"),
+            }
+        )
+
+        Subtitles[Track.attrib["lang_code"]] = {
+            "url": "https://www.youtube.com/api/timedtext?" + Params,
+            "ext": "srv1",
+        }
+
+    return Subtitles
+
+
+if __name__ == "__main__":
+    # testing code
+
+    import asyncio
+
+    print(asyncio.run(get_subtitle("")))
