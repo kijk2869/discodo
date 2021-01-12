a229e6e2252888707acc8c4aa3ee693592007fb6
eunwoo1104 <61371424+eunwoo1104@users.noreply.github.com>
2021-01-12 18:03:15 +0900

    Update discodo/voice_client.py
    Co-authored-by: Mary <mareowst@gmail.com>

--- discodo/voice_client.py
+++ discodo/voice_client.py
@@ -124,7 +124,9 @@ class VoiceClient(VoiceConnector):
         :rtype: str
         """
 
-        if not self.player or not self.Queue and not self.player.current:
+        if not self.player:
+            return "disconnected"
+        elif not self.Queue and not self.player.current:
             return "stopped"
         elif self.paused:
             return "paused"
