ff6f4846cbf2f5a9d42e60faba61d4722faff280
Mary <mareowst@gmail.com>
2021-02-28 16:56:33 +0900

    Add korean translation

--- docs/conf.py
+++ docs/conf.py
@@ -63,3 +63,8 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+
+# -- Options for sphinx-intl example
+
+locale_dirs = ["locale/"]
+gettext_compact = False
