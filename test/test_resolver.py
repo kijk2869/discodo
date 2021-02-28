98fcac2e31d099385bdcf8fe17cb61020525a96e
Mary <mareowst@gmail.com>
2021-02-28 12:02:46 +0900

    Fix unittest

--- test/test_resolver.py
+++ test/test_resolver.py
@@ -1,3 +1,4 @@
+import aiohttp
 import pytest
 
 from discodo.extractor.resolver import melon, vibe
@@ -5,28 +6,30 @@ from discodo.extractor.resolver import melon, vibe
 
 @pytest.mark.asyncio
 async def testMelon() -> None:
-    assert await melon.get_album(
-        "https://www.melon.com/album/detail.htm?albumId=10541761"
-    ) == ["쿠기 (Coogie) - POW (Feat. GRAY)"]
+    async with aiohttp.ClientSession as session:
+        assert await melon.get_album(
+            "https://www.melon.com/album/detail.htm?albumId=10541761", session
+        ) == ["쿠기 (Coogie) - POW (Feat. GRAY)"]
 
 
 @pytest.mark.asyncio
 async def testVibe() -> None:
-    assert await vibe.getChart("total")
+    async with aiohttp.ClientSession as session:
+        assert await vibe.getChart("total", session)
 
-    assert await vibe.getChart("genre-OS101")
+        assert await vibe.getChart("genre-OS101", session)
 
-    assert await vibe.getTrack("1") == "강산에 - 거꾸로 강을 거슬러 오르는 저 힘찬 연어들처럼"
+        assert await vibe.getTrack("1", session) == "강산에 - 거꾸로 강을 거슬러 오르는 저 힘찬 연어들처럼"
 
-    assert await vibe.getAlbum("1") == [
-        "강산에 - 거꾸로 강을 거슬러 오르는 저 힘찬 연어들처럼",
-        "강산에 - 미스템버린",
-        "강산에 - 코메디",
-        "강산에 - 억지",
-        "강산에 - 내마음의 구멍",
-        "강산에 - '98 아리랑",
-        "강산에 - 춤추는 나",
-        "강산에 - Cat Walk",
-        "강산에 - 맛!",
-        "강산에 - 나비의 입맞춤",
-    ]
+        assert await vibe.getAlbum("1", session) == [
+            "강산에 - 거꾸로 강을 거슬러 오르는 저 힘찬 연어들처럼",
+            "강산에 - 미스템버린",
+            "강산에 - 코메디",
+            "강산에 - 억지",
+            "강산에 - 내마음의 구멍",
+            "강산에 - '98 아리랑",
+            "강산에 - 춤추는 나",
+            "강산에 - Cat Walk",
+            "강산에 - 맛!",
+            "강산에 - 나비의 입맞춤",
+        ]
--- test/test_youtube.py
+++ test/test_youtube.py
@@ -1,3 +1,4 @@
+import aiohttp
 import pytest
 
 from discodo.extractor import youtube
@@ -5,25 +6,29 @@ from discodo.extractor import youtube
 
 @pytest.mark.asyncio
 async def testSearch() -> None:
-    assert await youtube.search("T7Myg0nHGzg") == [
-        {
-            "id": "T7Myg0nHGzg",
-            "title": "CS:GO CLUCH CLIP (2vs1)",
-            "webpage_url": "https://www.youtube.com/watch?v=T7Myg0nHGzg",
-            "uploader": "라고솔로가말했습니다",
-            "duration": 19,
-        }
-    ]
+    async with aiohttp.ClientSession() as session:
+        assert await youtube.search("T7Myg0nHGzg", session) == [
+            {
+                "id": "T7Myg0nHGzg",
+                "title": "CS:GO CLUCH CLIP (2vs1)",
+                "webpage_url": "https://www.youtube.com/watch?v=T7Myg0nHGzg",
+                "uploader": "라고솔로가말했습니다",
+                "duration": 19,
+            }
+        ]
 
 
 @pytest.mark.asyncio
 async def testPlaylist() -> None:
-    assert await youtube.extract_playlist("PLB6rrfCPynfApD_C0yItgW5WLC0f-wDvG") == [
-        {
-            "id": "T7Myg0nHGzg",
-            "title": "CS:GO CLUCH CLIP (2vs1)",
-            "webpage_url": "https://youtube.com/watch?v=T7Myg0nHGzg",
-            "uploader": "라고솔로가말했습니다",
-            "duration": "19",
-        }
-    ]
+    async with aiohttp.ClientSession() as session:
+        assert await youtube.extract_playlist(
+            "PLB6rrfCPynfApD_C0yItgW5WLC0f-wDvG", session
+        ) == [
+            {
+                "id": "T7Myg0nHGzg",
+                "title": "CS:GO CLUCH CLIP (2vs1)",
+                "webpage_url": "https://youtube.com/watch?v=T7Myg0nHGzg",
+                "uploader": "라고솔로가말했습니다",
+                "duration": "19",
+            }
+        ]
