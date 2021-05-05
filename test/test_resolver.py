21f303fe2210855983f83fb2a880fedc3c898c93
Mary <mareowst@gmail.com>
2021-05-05 12:10:04 +0900

    Fix resolver test

--- test/test_resolver.py
+++ test/test_resolver.py
@@ -9,7 +9,7 @@ async def testMelon() -> None:
     async with aiohttp.ClientSession() as session:
         assert await melon.get_album(
             "https://www.melon.com/album/detail.htm?albumId=10541761", session
-        ) == ["쿠기 (Coogie) - POW (Feat. GRAY)"]
+        ) == ['쿠기 (Coogie) - POW (Feat. GRAY) "topic"']
 
 
 @pytest.mark.asyncio
@@ -19,17 +19,18 @@ async def testVibe() -> None:
 
         assert await vibe.getChart("genre-OS101", session)
 
-        assert await vibe.getTrack("1", session) == "강산에 - 거꾸로 강을 거슬러 오르는 저 힘찬 연어들처럼"
+        assert await vibe.getTrack("1", session) == '강산에 - 거꾸로 강을 거슬러 오르는 저 힘찬 연어들처럼 "topic"'
 
         assert await vibe.getAlbum("1", session) == [
-            "강산에 - 거꾸로 강을 거슬러 오르는 저 힘찬 연어들처럼",
-            "강산에 - 미스템버린",
-            "강산에 - 코메디",
-            "강산에 - 억지",
-            "강산에 - 내마음의 구멍",
-            "강산에 - '98 아리랑",
-            "강산에 - 춤추는 나",
-            "강산에 - Cat Walk",
-            "강산에 - 맛!",
-            "강산에 - 나비의 입맞춤",
+            '강산에 - 거꾸로 강을 거슬러 오르는 저 힘찬 연어들처럼 "topic"',
+            '강산에 - 미스템버린 "topic"',
+            '강산에 - 코메디 "topic"',
+            '강산에 - 억지 "topic"',
+            '강산에 - 내마음의 구멍 "topic"',
+            '강산에 - \'98 아리랑 "topic"',
+            '강산에 - 춤추는 나 "topic"',
+            '강산에 - Cat Walk "topic"',
+            '강산에 - 맛! "topic"',
+            '강산에 - 나비의 입맞춤 "topic"',
         ]
+
